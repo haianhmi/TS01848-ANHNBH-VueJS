275e9445e8417a69419d38c3a33ed7b6ff52427a
haianhmi <anhnbhts01848@gmail.com>
2026-02-04 23:37:00 +0700

    Hoàn Thành Lab 8

---
+++ lab8/src/views/BlogPost.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.content }}</p>
+    <router-link to="/blog">Back to Blog List</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BlogPostView',
+  data() {
+    return {
+      posts: [
+        { id: 1, title: 'hello', content: 'oioioi' },
+        { id: 2, title: 'hi', content: 'loopy' },
+        { id: 3, title: 'halo', content: 'oioioivang' }
+      ],
+      post: {}
+    }
+  },
+  created() {
+    const postId = parseInt(this.$route.params.id)
+    this.post = this.posts.find(p => p.id === postId) || {}
+  }
+}
+</script>
---
+++ lab8/src/views/ProfileInfo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h3>Thông Tin</h3>
+    <p>Name: Nguyễn Bá Hải Anh</p>
+    <p>Email: anhnbhts01848@gmail.com</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfileInfoView'
+}
+</script>
